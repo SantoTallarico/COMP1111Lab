e581a1561f2e10ee9de59508df2c44694c8c5fc9
SantoTallarico <smansman@live.com>
2013-11-28 13:00:14 -0800

    Added java files

---
+++ Count.java
@@ -0,0 +1,22 @@
+package p1;
+//***********************************************
+//Count.java
+//Santo Tallarico
+//Counts the numbers 1-5 in 3 different languages
+//***********************************************
+public class Count {
+
+    public static void main(String[] args) {
+        
+        //English
+        System.out.println ("one two three four five");
+        
+        //French
+        System.out.println ("un deux trois quatre cinq");
+        
+        //Spanish
+        System.out.println ("uno dos tres cuatro cinco");
+
+    }
+
+}
---
+++ Simple.java
@@ -0,0 +1,10 @@
+package p1;
+
+public class Simple {
+
+    public static void main(String[] args) {
+        System.out.println ("Java rocks!!");
+
+    }
+
+}
